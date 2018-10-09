f468517c6df06fe5efc1551643390ce168fd957f
MarcoKruger <42533098+MarcoKruger@users.noreply.github.com>
2018-10-08 19:08:56 -0700

    First Commit

--- SolutionPrintClass/MainPage.xaml.cs
+++ SolutionPrintClass/MainPage.xaml.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Data.Pdf;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Printing;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +19,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Printing;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,5 +34,156 @@ namespace SolutionPrintClass
         {
             this.InitializeComponent();
         }
+        private PrintManager printMan;
+        private PrintDocument printDoc;
+        private IPrintDocumentSource printDocSource;
+
+        private StorageFile dest;
+
+        private async void StartPrint()
+        {
+            StorageFile file = null;
+            FileOpenPicker filePicker = new FileOpenPicker();
+            filePicker.FileTypeFilter.Add(".pdf");
+            filePicker.ViewMode = PickerViewMode.Thumbnail;
+            filePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            filePicker.SettingsIdentifier = "picker1";
+            filePicker.CommitButtonText = "Open Pdf File";
+
+            file = await filePicker.PickSingleFileAsync();
+
+            PdfDocument pdfDocument = await PdfDocument.LoadFromFileAsync(file);
+            if (pdfDocument != null && pdfDocument.PageCount > 0)
+            {
+                for (int pageIndex = 0; pageIndex < pdfDocument.PageCount; pageIndex++)
+                {
+                    var pdfPage = pdfDocument.GetPage((uint)pageIndex);
+                    if (pdfPage != null)
+                    {
+                        StorageFolder tempFolder = ApplicationData.Current.TemporaryFolder;
+                        StorageFile destinationFile = await KnownFolders.PicturesLibrary.CreateFileAsync(Guid.NewGuid().ToString() + ".jpg");
+                        dest = destinationFile;
+                        if (destinationFile != null)
+                        {
+                            //Output.Text = destinationFile.Path.ToString();
+                            IRandomAccessStream randomStream = await destinationFile.OpenAsync(FileAccessMode.ReadWrite);
+                            PdfPageRenderOptions pdfPageRenderOptions = new PdfPageRenderOptions();
+                            pdfPageRenderOptions.DestinationWidth = (uint)(this.ActualWidth - 130);
+                            await pdfPage.RenderToStreamAsync(randomStream, pdfPageRenderOptions);
+                            await randomStream.FlushAsync();
+                            randomStream.Dispose();
+                            pdfPage.Dispose();
+                        }
+                    }
+                }
+            }
+
+            // Code enters here
+            printMan = PrintManager.GetForCurrentView();
+            printMan.PrintTaskRequested += PrintTaskRequested;
+
+            // Build a PrintDocument and register for callbacks
+            printDoc = new PrintDocument();
+            printDocSource = printDoc.DocumentSource;
+            printDoc.Paginate += Paginate;
+            printDoc.GetPreviewPage += GetPreviewPage;
+            printDoc.AddPages += AddPages;
+
+            if (PrintManager.IsSupported())
+            {
+                try
+                {
+                    // Show print UI
+                    await PrintManager.ShowPrintUIAsync();
+                }
+                catch
+                {
+                    // Printing cannot proceed at this time
+                    ContentDialog noPrintingDialog = new ContentDialog()
+                    {
+                        Title = "Printing error",
+                        Content = "\nSorry, printing can' t proceed at this time.",
+                        PrimaryButtonText = "OK"
+                    };
+                    await noPrintingDialog.ShowAsync();
+                }
+            }
+            else
+            {
+                // Printing is not supported on this device
+                ContentDialog noPrintingDialog = new ContentDialog()
+                {
+                    Title = "Printing not supported",
+                    Content = "\nSorry, printing is not supported on this device.",
+                    PrimaryButtonText = "OK"
+                };
+                await noPrintingDialog.ShowAsync();
+            }
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StartPrint();
+        }
+
+        private void PrintTaskRequested(PrintManager sender, PrintTaskRequestedEventArgs args)
+        {
+            // Create the PrintTask.
+            // Defines the title and delegate for PrintTaskSourceRequested
+            var printTask = args.Request.CreatePrintTask("Print", PrintTaskSourceRequrested);
+
+            // Handle PrintTask.Completed to catch failed print jobs
+            printTask.Completed += PrintTaskCompleted;
+        }
+
+        private void PrintTaskSourceRequrested(PrintTaskSourceRequestedArgs args)
+        {
+            // Set the document source.
+            args.SetSource(printDocSource);
+        }
+
+
+        private void Paginate(object sender, PaginateEventArgs e)
+        {
+            // As I only want to print one Rectangle, so I set the count to 1
+            printDoc.SetPreviewPageCount(1, PreviewPageCountType.Final);
+        }
+
+        private void GetPreviewPage(object sender, GetPreviewPageEventArgs e)
+        {
+            // Provide a UIElement as the print preview.
+            //printDoc.SetPreviewPage(e.PageNumber, this.RectangleToPrint);
+        }
+
+        private void AddPages(object sender, AddPagesEventArgs e)
+        {
+            //printDoc.AddPage(this.RectangleToPrint);
+
+            // Indicate that all of the print pages have been provided
+            printDoc.AddPagesComplete();
+        }
+
+
+
+        private async void PrintTaskCompleted(PrintTask sender, PrintTaskCompletedEventArgs args)
+        {
+            // Notify the user when the print operation fails.
+            if (args.Completion == PrintTaskCompletion.Failed)
+            {
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                {
+                    ContentDialog noPrintingDialog = new ContentDialog()
+                    {
+                        Title = "Printing error",
+                        Content = "\nSorry, failed to print.",
+                        PrimaryButtonText = "OK"
+                    };
+                    await noPrintingDialog.ShowAsync();
+                });
+            }
+        }
     }
+}
 }
